3d1716cb569c6431c47bcfb7a416b10a15721012
박일규 <t5nn123@naver.com>
2021-11-08 01:25:57 +0900

    부트스트랩 예시

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 function App() {
-  return <div></div>;
+  return <Button variant="primary">Primary</Button>;
 }
 
 export default App;
